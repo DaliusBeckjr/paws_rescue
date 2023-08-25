0a9a8f31349e4370870044c4c5a0bd8f4dc99cb5
Dalius Beck <daliusbeckxiv@gmail.com>
2023-08-25 01:43:39 -0700

    Create test.js

---
+++ flask_app/static/js/test.js
@@ -0,0 +1,17 @@
+// API Key - zPtAtBdN
+// go here for more information on the api https://test1-api.rescuegroups.org/v5/public/docs
+
+// our fetch example from vue.js maybe try to figure out how to retrieve the data 
+// from this api while changing the interface. doesn't need a bearer token but a api
+// key to access the information
+// fetchData() {
+//     const path = "http://localhost:8000/ping";
+//     fetch(path, {method:"GET"})
+//     .then(response => response.json() )
+//     .then(data => {
+//         console.log(data)  //this will display data in our console
+//         this.msg = data
+//     })
+//     .catch(err => console.log(err.message))
+// }
+// },
